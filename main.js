c9014462c48e87da077ff520fbde7adcebca140a
Robert Pearce <robertwaltonpearce@gmail.com>
2015-03-26 00:06:13 -0600

    move app.js to main.js

--- main.js
+++ main.js
@@ -44,9 +44,4 @@ app.post('/send_email', function(request, response) {
   });
 });
 
-var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port)
-});
+module.exports = app;
